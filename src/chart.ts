f84a6d4742f8a982960e713b8ba55066ac9b7f7a
Søren Døssing <netcom@sauber.net>
2024-11-15 15:17:23 +0100

    Documentation

--- src/chart.ts
+++ src/chart.ts
@@ -4,12 +4,17 @@ import type { Index, Price } from "./types.ts";
  * Series of numbers
  * End is index of most recent value, usually 0
  * Start is index of oldest value and higher than end
- * */
+ * |start.............end|
+ * |N.N-1...........2.1.0|
+ */
 export class Chart {
+  /** Index of last item in array, more old entry */
   public readonly start: Index;
 
   constructor(
+    /** Series, which newest value first in array */
     public readonly series: Price[] = [],
+    /** Index of most recent (first in array) value */
     readonly end: Index = 0
   ) {
     this.start = this.end + this.series.length - 1;
@@ -22,7 +27,7 @@ export class Chart {
 
   /** Look up value at index */
   public val(index: Index): Price {
-    if (this.has(index)) return this.series[index-this.end];
+    if (this.has(index)) return this.series[index - this.end];
     throw new Error(
       `Chart index ${index} is outside range ${this.start}->${this.end}.`
     );
