283d991c252907df40d9fcce7f8dd16ccbfbab25
Søren Døssing <netcom@sauber.net>
2024-11-21 17:33:25 +0100

    Export required modules for running simulation

--- example.ts
+++ example.ts
@@ -1,12 +1,18 @@
-import { Simulation } from "./mod.ts";
-import type { Exchange } from "./src/exchange.ts";
-import type { Stats } from "./src/stats.ts";
+import { Simulation, type Strategy, type Exchange, type Stats } from "./mod.ts";
 import { makeExchange, TestStrategy } from "./src/testdata.ts";
 
+// Create an Exchange with a numebr instruments available
 const exchange: Exchange = makeExchange(5);
-const simulation = new Simulation(exchange, new TestStrategy());
+
+// Create Strategy
+const strategy: Strategy = new TestStrategy();
+
+// Run Simulation
+const simulation = new Simulation(exchange, strategy);
 simulation.run();
+const stats: Stats = simulation.stats;
+
+// Print list of transaction, positions and performance stats
 console.log(simulation.account.statement);
 console.log(simulation.account.portfolio.statement(exchange.end));
-const stats: Stats = simulation.stats;
 console.log(`Performance. Profit: ${(stats.profit*100).toFixed(2)}% Trades: ${stats.trades.length} Omega Ratio: ${stats.omegaRatio.toFixed(2)}`)
--- mod.ts
+++ mod.ts
@@ -1,2 +1,4 @@
 export * from "./src/simulation.ts";
+export * from "./src/exchange.ts";
+export * from "./src/stats.ts";
 export * from "./src/types.ts";
