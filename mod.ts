0c57c62e3e3dda587c9192f65a6b2ae5de5f54b2
Søren Døssing <netcom@sauber.net>
2024-11-22 12:42:20 +0100

    export positions type

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
+export * from "./src/types.ts";
 export * from "./src/simulation.ts";
 export * from "./src/exchange.ts";
 export * from "./src/stats.ts";
-export * from "./src/types.ts";
+export * from "./src/position.ts";
