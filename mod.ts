5abecd2a3e6d504f15f213b113574f5836605d74
Søren Døssing <netcom@sauber.net>
2024-12-09 12:18:41 +0100

    Expert portfolio

--- mod.ts
+++ mod.ts
@@ -4,4 +4,5 @@ export * from "./src/exchange.ts";
 export * from "./src/stats.ts";
 export * from "./src/position.ts";
 export * from "./src/instrument.ts";
+export * from "./src/portfolio.ts";
 export * from "./src/testinstrument.ts";
